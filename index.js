daa2e97b1b9551e203e3fa3eba9d0677ea91baeb
Laura Siebert <laurasiebert80@gmail.com>
2018-09-11 14:35:16 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,137 @@
+const questionBank = [
+    {
+        questionLocation: 0,
+        question: 'What is the phrase called within the letra, where the singer is not singing, and 1-2 compas is available for a desplante?',
+        ans1: 'Aliento',
+        ans2: 'Respiro',
+        ans3: 'Rabieta',
+        ans4: 'Descanso'
+    },
+    {
+        questionLocation: 1,
+        question: 'Across all palos, what is the structure of the melody?',
+        ans1: '1 compas for both the melody and resolution of the melody.',
+        ans2: '6 beats for the melody and 6 beats for the resolution',
+        ans3: 'There is no structure to the melody.',
+        ans4: '1 compas to develop the melody and 1 compas for the resolution of the melody.'
+    },
+    {
+        questionLocation: 2,
+        question: 'Especially in a tablao, why is it so important to do a recoje before every llamada, major desplante, and escobilla?',
+        ans1: 'The singer and guitarist also start on the 1. If there is no signal, there can be a conflict.',
+        ans2: 'It sounds better.',
+        ans3: 'Usually recojes include really sharp pellizcos that get the audience excited.',
+        ans4: 'It gives the dancer a time to rest before the intense parts of the piece.'
+    },
+    {
+        questionLocation: 3,
+        question: 'At the time that you have little to no rehearsals with the musicians, what is the single most important thing to establish?',
+        ans1: 'The speed of the piece.',
+        ans2: 'The length of the piece. ',
+        ans3: 'The structure of the piece.',
+        ans4: 'The letras of the piece.'
+    },
+    {
+        questLocation: 4,
+        question: 'What is a basic structure for most palos?',
+        ans1: 'Intro, Letra, Falseta, Letra, Escobilla, Letra, Coletilla',
+        ans2: 'Letra, Falseta, Letra, Escobilla, Letra, Coletilla',
+        ans3: 'Intro, Letra, Falseta, Letra, Escobilla, Letra, Estribillo/Salida.',
+        ans4: 'Intro, Letra, Falseta, Macho, Escobilla, Letra, Estribillo/Salida.'
+    },
+    {
+        questLocation: 5,
+        question: 'In a tablao setting, when you are done with your piece, what should you do?',
+        ans1: 'Go the the dressing room/bathroom to stretch.',
+        ans2: "Catch your breath and support the next artist's piece with jaleo and palmas.",
+        ans3: 'Head the the dressing room/bathroom to reapply makeup and do a costume change.',
+        ans4: "Sit in your spot and do palmas softly and do jaleo, because typically you don't know exactly what the other artists will be doing."
+    },
+    {
+        questLocation: 6,
+        question: 'During fin de fiesta, what is the expected protocol?',
+        ans1: 'Let the singer sing a few letras to start.',
+        ans2: 'The singer or director of the show will look at you for your turn to dance.',
+        ans3: 'Dance a SHORT pataita with no long escobillas (unless you are the guest artist). Get in and get out.',
+        ans4: 'All of the above.'
+    },
+    {
+        questLocation: 7,
+        question: 'Is improvisation a requirement to dance in a tablao setting?',
+        ans1: 'Yes, to a point; You should be comfortable enough with the compas to move from mistake to recovery smoothly.',
+        ans2: 'No. All dances should be set, rehearsed, and choreographed.',
+        ans3: 'Yes. In a tablao setting, you should be ready to dance any palo, with any structure, at any time.',
+        ans4: 'Yes. You should be able to improvise a complete dance in order to start dancing in a tablao setting.'
+    },
+    {
+        questLocation: 8,
+        question: 'What is the job of the dancer in a tablao setting?',
+        ans1: 'Dancers are responsible for mounting and directing their own pieces.',
+        ans2: 'Dancer are responsible for mounting and directing their own pieces, sound checking the footwork, doing palmas throughout the show, doing jaleo throughout the show, and supporting the entire cuadro to their best ability.',
+        ans3: 'Dancers are responsible for whatever the singer assigns them to do.',
+        ans4: 'Dancers are responsible for loading in and out, sound checking the footwork, and mounting and directing their own pieces.'
+    },
+    {
+        questLocation: 9,
+        question: 'What is the best way to improve your skills for dancing in a tablao?',
+        ans1: 'Focus on the perfecting each choreography from start to finish and in order.',
+        ans2: 'Work on increasing the difficulty of your pieces.',
+        ans3: 'Dance in a tablao setting as much as you can, understanding that mistakes will be made, but are also learning opportunites.',
+        ans4: 'Practice staying focused on you your choreography and structure, regardless of what the musicians are doing.'
+    }
+]
+
+// Render Quiz Questions and Structure ===================
+function renderQuiz(){
+
+}
+
+// Buttons ===========================
+
+function handleStartButton(){
+
+}
+
+function handleSubmitButton(){
+
+}
+
+function handleNextButton(){
+
+}
+
+function handleRestartButton() {
+
+}
+
+// Actions =============================
+function nextQuestion(){
+
+}
+
+// Render information ==================
+function renderResults(){
+
+}
+
+function renderScore(){
+
+}
+
+function renderLocation(){
+
+}
+
+// Feedback handling ====================
+function incorrectAnswer(){
+
+}
+
+function correctAnswer(){
+
+}
+
+// Render Quiz in it's entirety ===========
+function createQuiz(){
+
+}
